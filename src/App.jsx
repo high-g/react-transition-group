7c0f9452b37b5635c0a0adf5b55a4a6a126a9043
fumiya <altema1217bbfumi@live.jp>
2021-11-01 16:06:34 +0900

    feat: react-transiton-group使い方更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
+import { useState } from 'react'
 import { css } from '@emotion/react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 function App() {
+  const [list, setList] = useState([1, 2, 3, 4, 5])
+
+  const handlePush = () => {
+    setList([...list, Math.floor(Math.random() * 11)])
+  }
+
   return (
-    <div>
-      <p css={pCss}>test</p>
-    </div>
+    <>
+      <button onClick={handlePush}>要素を追加</button>
+
+      <TransitionGroup>
+        {list.map((data, i) => (
+          <CSSTransition key={i} timeout={0} unmountOnExit>
+            <div css={transitionStyle}>{data}</div>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </>
   )
 }
 
-const pCss = css`
-  color: red;
+const transitionStyle = css`
+  &.enter-active {
+    opacity: 0;
+  }
+
+  &.enter-done {
+    transition: opacity 0.3s;
+    opacity: 1;
+  }
 `
 
 export default App
